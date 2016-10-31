4fc5bee951e48eb8413d5f293110e5e9b7d5fd65
Steven Orvis <steven.orvis@geneva.edu>
2016-10-31 18:09:22 -0400

    changed deploy.rb to set app name

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'C.h.i.p. Grarage Door'
+set :repo_url, 'git@github.com:sorvis/C.h.i.p.-Garage-Door.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+ set :deploy_to, '/home/chip/garage_door'
 
 # Default value for :scm is :git
 # set :scm, :git
