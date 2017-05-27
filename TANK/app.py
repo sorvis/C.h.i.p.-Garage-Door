de106fc21a6e8a95807cc59053199eda2f6f764b
caleb <cctanks@gmail.com>
2017-05-27 18:49:47 +0000

    LED

--- TANK/app.py
+++ TANK/app.py
@@ -9,6 +9,18 @@ def index():
 @app.route('/cakes')
 def cakes():
     return 'Yummy cakes!'
+@app.route('/forwardon')
+def forwardon():
+import RPi.GPIO as GPIO ## Import GPIO library
+GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
+GPIO.setup(40, GPIO.OUT) ## Setup GPIO Pin 40 to OUT
+GPIO.output(40,True) ## Turn on GPIO pin 40
+@app.route('/forwardoff')
+def forwardoff
+import RPi.GPIO as GPIO ## Import GPIO library
+GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
+GPIO.setup(40, GPIO.OUT) ## Setup GPIO Pin 40 to OUT
+GPIO.output(40,false) ## Turn off GPIO pin 40
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
