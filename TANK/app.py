9e8f43bef92b23407a5a191998a6d94386575fb3
Steven Orvis <steven@trappingpa.com>
2017-05-27 14:52:13 -0400

    Added proper indents and moved inports

--- TANK/app.py
+++ TANK/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+import RPi.GPIO as GPIO ## Import GPIO library
 
 
 app = Flask(__name__)
+GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
 
 @app.route('/')
 def index():
@@ -11,16 +13,12 @@ def cakes():
     return 'Yummy cakes!'
 @app.route('/forwardon')
 def forwardon():
-import RPi.GPIO as GPIO ## Import GPIO library
-GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-GPIO.setup(40, GPIO.OUT) ## Setup GPIO Pin 40 to OUT
-GPIO.output(40,True) ## Turn on GPIO pin 40
+    GPIO.setup(40, GPIO.OUT) ## Setup GPIO Pin 40 to OUT
+    GPIO.output(40,True) ## Turn on GPIO pin 40
 @app.route('/forwardoff')
 def forwardoff
-import RPi.GPIO as GPIO ## Import GPIO library
-GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-GPIO.setup(40, GPIO.OUT) ## Setup GPIO Pin 40 to OUT
-GPIO.output(40,false) ## Turn off GPIO pin 40
+    GPIO.setup(40, GPIO.OUT) ## Setup GPIO Pin 40 to OUT
+    GPIO.output(40,false) ## Turn off GPIO pin 40
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
